5deb5732a8bac924d6022e4a89684dd0737144a2
hardmarlin <hardmarlin@yahoo.com>
2011-12-07 12:18:13 -0300

    Ran the cucumber and rspec generators

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ficha::Application.config.secret_token = 'f7d5a942c9e7de4c43401b34fc51526a7e93b352ea0d8cd9fc3cbb8f41cffcdb1904a2f51494741a693fdfa7d2436b38170dd50cd4c6594cac0d0129b86ede33'
